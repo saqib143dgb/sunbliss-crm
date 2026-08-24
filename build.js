17ab260fe5db51e47c89af743ca298c4c76d2cab
saqib143dgb <saqibzeya88@gmail.com>
2026-08-24 17:43:49 +0400

    Restore original Purvanchal logo (#73)
    * Restore original uploaded Purvanchal logo
    
    * Load original brand logo patch last

--- build.js
+++ build.js
@@ -49,7 +49,8 @@ const LOCAL_PATCH_FILES = [
   'premium_hero_header_patch.js',
   'premium_hero_header_refine_patch.js',
   'header_image_edge_fix_patch.js',
-  'header_curve_remove_patch.js'
+  'header_curve_remove_patch.js',
+  'original_brand_logo_patch.js'
 ];
 
 async function download(file, required = true) {
