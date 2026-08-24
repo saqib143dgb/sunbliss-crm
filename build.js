9c27bdb172f8d6c5dceb92a929955c8798aa0147
saqib143dgb <saqibzeya88@gmail.com>
2026-08-24 11:28:23 +0400

    Load persistent back control last

--- build.js
+++ build.js
@@ -31,7 +31,8 @@ const LOCAL_PATCH_FILES = [
   'compliance_editor_patch.js',
   'action_required_patch.js',
   'sequenced_payment_labels_patch.js',
-  'extra_installments_patch.js'
+  'extra_installments_patch.js',
+  'persistent_back_patch.js'
 ];
 
 async function download(file, required = true) {
