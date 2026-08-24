d9e01b03369b6de9db0190eb870a1d344b346c54
saqib143dgb <saqibzeya88@gmail.com>
2026-08-24 16:42:55 +0400

    Load premium header refinement last

--- build.js
+++ build.js
@@ -46,7 +46,8 @@ const LOCAL_PATCH_FILES = [
   'header_asset_building_b.js',
   'header_asset_logo_a.js',
   'header_asset_logo_b.js',
-  'premium_hero_header_patch.js'
+  'premium_hero_header_patch.js',
+  'premium_hero_header_refine_patch.js'
 ];
 
 async function download(file, required = true) {
