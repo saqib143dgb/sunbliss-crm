81b58ad116c1088182aa6715190057af575d3d15
saqib143dgb <saqibzeya88@gmail.com>
2026-08-24 10:27:54 +0400

    Load compliance editor refinement last

--- build.js
+++ build.js
@@ -24,7 +24,8 @@ const LOCAL_PATCH_FILES = [
   'mobile_input_zoom_patch.js',
   'header_spacing_patch.js',
   'unit_detail_workflow_patch.js',
-  'detail_action_cleanup_patch.js'
+  'detail_action_cleanup_patch.js',
+  'compliance_editor_patch.js'
 ];
 
 async function download(file, required = true) {
@@ -50,7 +51,7 @@ async function main() {
 
   for (const file of OPTIONAL_BINARY_FILES) {
     const body = await download(file, false);
-    if (body) fs.writeFileSync(path.join(OUT, file), body);
+    if (body) fs.writeFileSync(path.join(OUT, file));
   }
 
   for (const file of LOCAL_PATCH_FILES) {
