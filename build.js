b94dfa76f5cadef661fe66908c543a6ae3fe99f9
saqib143dgb <saqibzeya88@gmail.com>
2026-08-24 12:09:36 +0400

    Load installment editor patch last

--- build.js
+++ build.js
@@ -35,7 +35,8 @@ const LOCAL_PATCH_FILES = [
   'persistent_back_patch.js',
   'new_customer_sales_channel_patch.js',
   'furnishing_type_patch.js',
-  'furnishing_refresh_patch.js'
+  'furnishing_refresh_patch.js',
+  'installment_edit_patch.js'
 ];
 
 async function download(file, required = true) {
