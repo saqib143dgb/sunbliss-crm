b01d4e070c344673005fc3b43d13c77db894add6
saqib143dgb <saqibzeya88@gmail.com>
2026-08-27 19:20:54 +0400

    Remove broad iOS cancel workaround after fixing render loop

--- build.js
+++ build.js
@@ -82,8 +82,7 @@ const LOCAL_PATCH_FILES = [
   'full_width_print_buttons_patch.js',
   'combined_brand_header_patch.js',
   'payment_percentage_admin_exclusion_patch.js',
-  'cancel_unit_hang_fix_patch.js',
-  'cancel_unit_ios_render_fix.js'
+  'cancel_unit_hang_fix_patch.js'
 ];
 
 function requireFile(filePath, label) {
