b7e9583312d0328bb5ad27adf642ec2297ca7a84
saqib143dgb <saqibzeya88@gmail.com>
2026-08-27 22:54:24 +0400

    Reset header stack to single professional header

--- build.js
+++ build.js
@@ -71,8 +71,7 @@ const LOCAL_PATCH_FILES = [
   'bold_headings_patch.js',
   'credit_note_edit_patch.js',
   'unit_meta_inline_patch.js',
-  'fresh_reference_header_patch.js',
-  'fresh_reference_header_mobile_match_patch.js'
+  'professional_header_patch.js'
 ];
 
 function requireFile(filePath, label) {
