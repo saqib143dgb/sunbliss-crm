91ccdb823ae3893ebd32719b24f8ed493a6ccb4f
saqib143dgb <saqibzeya88@gmail.com>
2026-08-28 16:40:46 +0400

    Load Notes management patch last

--- build.js
+++ build.js
@@ -82,7 +82,8 @@ const LOCAL_PATCH_FILES = [
   'customer_notes_stability_patch.js',
   'customer_note_display_cleanup_patch.js',
   'sale_compliance_inline_note_hide_patch.js',
-  'issued_credit_note_history_patch.js'
+  'issued_credit_note_history_patch.js',
+  'notes_management_patch.js'
 ];
 
 function requireFile(filePath, label) {
