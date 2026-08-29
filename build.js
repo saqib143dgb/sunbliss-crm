cd5df3fe9428a9f25f4ac2941a740472e9182ea7
saqib143dgb <saqibzeya88@gmail.com>
2026-08-29 22:21:05 +0400

    Load Extensions filter freeze fix

--- build.js
+++ build.js
@@ -95,6 +95,7 @@ const LOCAL_PATCH_FILES = [
   'payment_extensions_core_patch.js',
   'payment_extensions_ui_patch.js',
   'payment_extensions_uncovered_overdue_patch.js',
+  'scheduled_actions_extension_filter_freeze_fix.js',
   'scheduled_actions_full_page_guard.js',
   'full_page_action_workflow_patch.js'
 ];
