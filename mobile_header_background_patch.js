b124f2788cd921beaaef89582499a1c016a0ba08
saqib143dgb <saqibzeya88@gmail.com>
2026-09-01 20:39:55 +0400

    Move mobile project row down 10px

--- mobile_header_background_patch.js
+++ mobile_header_background_patch.js
@@ -221,7 +221,7 @@
 
       .topbar.sunbliss-professional-header .sb-pro-project-row{
         min-width:0!important;
-        margin-top:20px!important;
+        margin-top:30px!important;
         position:relative!important;
         isolation:isolate!important;
         display:flex!important;
